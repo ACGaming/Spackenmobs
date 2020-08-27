ea518fd4d4f3c3191d93aead1729a0d715762db9
Hendrik <nglbnm@web.de>
2020-08-27 20:22:05 +0200

    Fix arbitrary config formatting

--- src/main/java/mod/acgaming/spackenmobs/misc/ModItems.java
+++ src/main/java/mod/acgaming/spackenmobs/misc/ModItems.java
@@ -2,13 +2,7 @@ package mod.acgaming.spackenmobs.misc;
 
 import mod.acgaming.spackenmobs.Spackenmobs;
 import mod.acgaming.spackenmobs.items.ItemBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionEffect;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 
 @ObjectHolder(Spackenmobs.MODID)
@@ -17,17 +11,4 @@ public class ModItems
 	public static final Item RAM = new ItemBase("ram", Spackenmobs.SPACKENMOBS_TAB);
 	public static final Item RAM_ON_A_STICK = new ItemBase("ram_on_a_stick", Spackenmobs.SPACKENMOBS_TAB);
 	public static final Item SURSTROEMMING = new ItemBase("surstroemming", Spackenmobs.SPACKENMOBS_TAB);
-
-	@SubscribeEvent
-	public void SurstroemmingEvent(PlayerTickEvent event)
-	{
-		if (event.player instanceof EntityPlayer)
-		{
-			EntityPlayer player = event.player;
-			if (player.inventory.hasItemStack(new ItemStack(ModItems.SURSTROEMMING)))
-			{
-				player.addPotionEffect(new PotionEffect(MobEffects.NAUSEA, 100));
-			}
-		}
-	}
 }
